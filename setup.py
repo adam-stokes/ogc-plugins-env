3ed88ec439e5e0b7ae2f150ec7e13b51c56cd7eb
Adam Stokes <battlemidget@users.noreply.github.com>
2019-07-28 00:41:45 -0400

    library updates
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+import ogc_plugins_env as package
 from pathlib import Path
 
 README = Path(__file__).parent.absolute() / "readme.md"
@@ -6,17 +7,17 @@ README = README.read_text(encoding="utf8")
 
 setuptools.setup(
     name="ogc-plugins-env",
-    version="0.0.6",
-    author="Adam Stokes",
-    author_email="adam.stokes@ubuntu.com",
-    description="ogc-plugins-env, a ogc plugin for environment discovery",
+    version=package.__version__,
+    author=package.__author__,
+    author_email=package.__author_email__,
+    description=package.__description__,
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/battlemidget/ogc-plugin-env",
-    packages=["ogc_plugins_env"],
+    url=package.__git_repo__,
+    py_modules=["ogc_plugins_env"],
     entry_points={"ogc.plugins": "Env = ogc_plugins_env:Env"},
     install_requires=[
-        "ogc>=0.1.5,<1.0.0",
+        "ogc>=0.3.0,<1.0.0",
         "click>=7.0.0,<8.0.0",
         "python-dotenv>=0.10.3,<1.0.0",
     ],
